50be704d166f03cabf5d8925a5c0f65cb285ac74
Celso <celso.machado@etec.sp.gov.br>
2024-08-15 02:30:53 -0300

    inicio da pagina contatos
    Falta o metodo Post  para o envio da mensagem para a coordenacao, ou seja la para onde for a mensagem

---
+++ src/app/contato.tsx
@@ -0,0 +1,112 @@
+import Head from "next/head";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+function Contato() {
+	const [erro, setErro] = useState("");
+	const [sucesso, setSucesso] = useState("");
+	const router = useRouter();
+
+	const valoresIniciais = {
+		nome: "",
+		email: "",
+		mensagem: "",
+	};
+	
+	const validacaoSchema = Yup.object().shape({
+		nome: Yup.string().required("Campo obrigatorio"),
+		email: Yup.string().email("Email invalido").required("Campo obrigatorio"),
+		mensagem: Yup.string().required("Campo obrigatorio"),
+		});
+
+	const handleSubmit = async (valores, {setSubmitting}) => {
+		setErro("");
+		setSucesso("");
+
+// continuacao do envio da imagem, necessario construir a API
+
+	};
+	
+	return (
+		<>
+	 <Head>
+		<title> CertTarefas </title>
+	</Head>
+	<Header />
+		<main className="container mx-auto p-4">
+			<h2 className="text-3x1">
+				Entre em Contato Conosco
+			</h2>
+			<Formik
+				initialValues = {valoresIniciais}
+				validationSchema = {validacaoSchema}
+				onSubmit = {handleSubmit}
+			>
+			{({ isSubmitting }) => (
+			<Form 
+			className="shadow appearance-none border rounded
+			w-full py-2 px-3 text-gray-700 leading-tight
+			focus:outline-none focus:shadow-outline">
+				<div className= "mb-4">
+					<label htmlFor="username"
+		className="block text-gray-700 text-sm font-bold mb-2">	
+					Nome
+					</label>
+				<Field
+					type="text"
+					name="nome"
+					className="form-control"
+					placeholder="Nome"
+				/>
+				<ErrorMessage
+					name="Nome"
+					component= "text-danger"
+				/>
+				</div>
+
+				<div className="mb-4">
+					<label htmlFor="email"
+		className="block text-gray-700 text-sm font-bold mb-2">
+					Email
+					</label>
+				<Field
+					type="email"
+					name="email"
+					className="form-control"
+					placeholder="nome@dominio.com"
+				/>
+				<ErrorMessage
+					name="email"
+					component="div"
+					className="text-danger"
+				/>
+				</div>
+
+				<div className="mb-4">
+					<label htmlFor="message">
+						Mensagem
+					</label>
+				<Field
+					type="text"
+					name="mensagem"
+					className="fom-control"
+					placeholder="Sua mensagem"
+				/>
+				<ErrorMessage
+					name="mensagem"
+					component="div"
+					className="text-dange"
+				/>
+				</div>
+			</Form>
+				)}
+			</Formik>
+			</main>
+			</>)
+		}
+
+export default Contato;
