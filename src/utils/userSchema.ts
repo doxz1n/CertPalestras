96f4cb791dc5a95b6eb9a34c92f13b23454e4b8f
Henrique Sanchez <henriquealcici@gmail.com>
2024-08-22 00:01:45 -0300

    Cria o Schema de Usuário

---
+++ src/utils/userSchema.ts
@@ -0,0 +1,50 @@
+import * as Yup from "yup";
+
+// Define a interface para o usuário
+interface EventoInscrito {
+  eventoId: string;
+  presencaValidada: boolean;
+}
+
+interface Certificado {
+  certificadoId: string;
+  emitidoEm: Date;
+}
+
+//
+export interface Usuario {
+  uid: string;
+  email: string;
+  nome: string;
+  tipo: "aluno" | "coordenador";
+  eventosInscritos: EventoInscrito[];
+  certificados: Certificado[];
+}
+
+// Define o schema de validação usando Yup
+const userSchema: Yup.ObjectSchema<Usuario> = Yup.object().shape({
+  uid: Yup.string().required(),
+  email: Yup.string().email().required(),
+  nome: Yup.string().required(),
+  tipo: Yup.mixed<"aluno" | "coordenador">()
+    .oneOf(["aluno", "coordenador"])
+    .required(),
+  eventosInscritos: Yup.array()
+    .of(
+      Yup.object().shape({
+        eventoId: Yup.string().required(),
+        presencaValidada: Yup.boolean().required(),
+      })
+    )
+    .required(),
+  certificados: Yup.array()
+    .of(
+      Yup.object().shape({
+        certificadoId: Yup.string().required(),
+        emitidoEm: Yup.date().required(),
+      })
+    )
+    .required(),
+});
+
+export default userSchema;
