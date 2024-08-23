44d7552baafe682feddbd792e3b1de873fb0881f
Henrique Sanchez <henriquealcici@gmail.com>
2024-08-22 23:57:06 -0300

    Criação de API POST para criação de usuarios (coordenador e aluno)

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,71 @@
+import { db } from "@/../lib/firebase";
+import userSchema, { Usuario } from "@/utils/userSchema";
+import { collection, addDoc } from "firebase/firestore";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { NextResponse } from "next/server";
+import * as Yup from "yup";
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    // Validação dos dados recebidos com Yup
+    const validatedUserData: Usuario = await userSchema.validate(body, {
+      abortEarly: false,
+    });
+    // Criação de um novo usuário no Firebase Authentication
+    if (validatedUserData.tipo === "coordenador") {
+      const auth = getAuth();
+
+      //Criação do coordenador no Firebase Auth usando email e senha
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        validatedUserData.email,
+        validatedUserData.senha
+      );
+
+      const uid = userCredential.user.uid;
+
+      await addDoc(collection(db, "coordenadores"), {
+        uid,
+        email: validatedUserData.email,
+        nome: validatedUserData.nome,
+        eventosInscritos: validatedUserData.eventosInscritos,
+        certificados: validatedUserData.certificados,
+      });
+
+      return NextResponse.json(
+        { message: "Coordenador criado com sucesso!" },
+        { status: 200 }
+      );
+    } else if (validatedUserData.tipo === "aluno") {
+      //Adiciona Aluno ao firestore
+      await addDoc(collection(db, "alunos"), {
+        email: validatedUserData.email,
+        nome: validatedUserData.nome,
+        eventosInscritos: validatedUserData.eventosInscritos,
+        certificados: validatedUserData.certificados,
+      });
+
+      return NextResponse.json(
+        { message: "Aluno criado com sucesso!" },
+        { status: 200 }
+      );
+    } else {
+      return NextResponse.json(
+        { error: "Tipo de usuário inválido" },
+        { status: 400 }
+      );
+    }
+  } catch (error) {
+    // Verifica se o erro é uma instância de Yup.ValidationError, que ocorre se a validação dos dados falhar.
+    if (error instanceof Yup.ValidationError) {
+      return NextResponse.json({ error: error.errors }, { status: 400 });
+    }
+    // Se o erro não for de validação, ele é registrado no console para depuração.
+    console.error("Erro ao criar usuário:", error);
+    return NextResponse.json(
+      { error: "Erro interno do servidor" },
+      { status: 500 }
+    );
+  }
+}
--- src/utils/userSchema.ts
+++ src/utils/userSchema.ts
@@ -13,19 +13,21 @@ interface Certificado {
 
 //
 export interface Usuario {
-  uid: string;
   email: string;
   nome: string;
   tipo: "aluno" | "coordenador";
+  senha: string;
   eventosInscritos: EventoInscrito[];
   certificados: Certificado[];
 }
 
 // Define o schema de validação usando Yup
 const userSchema: Yup.ObjectSchema<Usuario> = Yup.object().shape({
-  uid: Yup.string().required(),
   email: Yup.string().email().required(),
   nome: Yup.string().required(),
+  senha: Yup.string()
+    .min(6, "A senha deve ter pelo menos 6 caracteres")
+    .required(),
   tipo: Yup.mixed<"aluno" | "coordenador">()
     .oneOf(["aluno", "coordenador"])
     .required(),
