1adfcab3266602642f6506a3f21b2063c39933e3
Henrique Sanchez <henriquealcici@gmail.com>
2024-08-27 19:35:55 -0300

    Corrige o componente Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,5 @@
 import React from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="bg-slate-100 text-black flex flex-col min-h-screen">
-      {children}
-    </div>
-  );
+  return <div className="bg-slate-100 text-black min-h-screen">{children}</div>;
 }
