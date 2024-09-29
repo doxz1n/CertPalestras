02a9ae8c7232c10541769654b6eeddd159ee192e
Henrique Sanchez <henriquealcici@gmail.com>
2024-09-29 18:10:32 -0300

    Conflitos

--- src/app/inscricao/[id]/page.tsx
+++ src/app/inscricao/[id]/page.tsx
@@ -1,52 +1,33 @@
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import moment from "moment";
-import { fetchEventobyId } from "../../../../lib/actions";
-import type { Evento } from "@/utils/eventoSchema";
+import InscricaoForm from "@/components/InscricaoForm";
+import { obterEventoPorId } from "@/../lib/actions"; // Certifique-se de que o caminho está correto
+import { Evento } from "@/utils/eventoSchema";
 
-export default async function Page({ params }: { params: { id: string } }) {
-  const id = params.id;
-  const evento = await fetchEventobyId(id);
-  if (evento) {
-    return (
-      <div className="max-w-2xl mx-auto p-6 bg-white shadow-md rounded-lg">
-        <h1 className="text-2xl font-semibold mb-4">
-          Inscrição para {evento.nome}
-        </h1>
-        <p>{evento.descricao}</p>
-        <p>
-          <strong>Início:</strong>{" "}
-          {moment(evento.dataInicio).format("DD/MM/YYYY HH:mm")}
-        </p>
-        <p>
-          <strong>Fim:</strong>{" "}
-          {moment(evento.dataFim).format("DD/MM/YYYY HH:mm")}
-        </p>
-        <p>
-          <strong>Vagas Disponíveis:</strong> {evento.vagas}
-        </p>
-        <form className="mt-4">
-          <div className="mb-4">
-            <label className="block text-gray-700">Nome:</label>
-            <input
-              type="text"
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none"
-              placeholder="Seu nome"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700">Email:</label>
-            <input
-              type="email"
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none"
-              placeholder="Seu email"
-            />
-          </div>
-          <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">
-            Confirmar Inscrição
-          </button>
-        </form>
-      </div>
-    );
-  }
+interface EventoPageProps {
+  params: {
+    id: string;
+  };
 }
+
+const EventoPage = async ({ params }: EventoPageProps) => {
+  // Usando a função obterEventoPorId diretamente
+  const evento = await obterEventoPorId(params.id);
+
+  if (!evento) {
+    return <p>Evento não encontrado {params.id}</p>;
+  }
+
+  return (
+    <div>
+      <h1>{evento.nome}</h1>
+      <p>{evento.descricao}</p>
+      <p>
+        Data: {evento.dataInicio} - {evento.dataFim}
+      </p>
+      <p>Vagas: {evento.vagas}</p>
+
+      <InscricaoForm eventoId={evento.id!} />
+    </div>
+  );
+};
+
+export default EventoPage;
