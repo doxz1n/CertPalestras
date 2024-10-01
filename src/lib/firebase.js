32330f2f7360d9a10db2cf0740a48d10189a59d4
Henrique Sanchez <henriquealcici@gmail.com>
2024-10-01 15:06:12 -0300

    Erros

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,21 +1,21 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth"; // AUTH
+import { getAuth } from "firebase/auth";
 
 // configuração do Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBSx2ctNkhUtbnS1MitCN7sgbgq3wiXgkI",
-  authDomain: "certpalestras.firebaseapp.com",
-  projectId: "certpalestras",
-  storageBucket: "certpalestras.appspot.com",
-  messagingSenderId: "931185678644",
-  appId: "1:931185678644:web:eaaa37326d09fddb4a2d61",
-  measurementId: "G-WKT68VVE7H",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
 
 // Inicializa o Firebase
-const app = initializeApp(firebaseConfig);
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
-const auth = getAuth(app); // Se você precisar de autenticação
+const auth = getAuth(app);
 
 export { db, auth };
