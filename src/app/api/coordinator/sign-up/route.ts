1f2e27134660a3f0babd219d373152dee0de8a73
Henrique Sanchez <henriquealcici@gmail.com>
2024-10-08 20:17:02 -0300

    Correções

--- src/app/api/coordinator/sign-up/route.ts
+++ src/app/api/coordinator/sign-up/route.ts
@@ -3,7 +3,11 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import { setDoc, doc } from "firebase/firestore";
 import { NextResponse } from "next/server";
 
-export async function POST(request: Request) {
+export async function POST(
+  request: Request,
+  statusCode: number,
+  responseMessage: string
+) {
   try {
     const { nome, email, cpf, senha } = await request.json();
     //Criação do coordenador no Firebase Auth usando email e senha
@@ -27,23 +31,26 @@ export async function POST(request: Request) {
     );
   } catch (error: any) {
     console.error("Erro ao criar usuário:", error);
-    
+
     let errorMessage = "Erro desconhecido ao criar o usuario.";
     if (error instanceof Error) {
-	//tratamento padrao
-	errorMessage = error.message;
-    } else if ( typeof error === 'string') {
-	errorMessage = error;
+      //tratamento padrao
+      errorMessage = error.message;
+    } else if (typeof error === "string") {
+      errorMessage = error;
     } else if (error?.code) {
-	switch (error.code) {
-		case 'auth/email-already-in-use':
-			errorMessage = 'Este email ja esta sendo utilizado';
-			break;
-		case 'auth/internal-error':
-			errorMessage = 'Erro no Servidor';
-			break;
-   }
+      switch (error.code) {
+        case "auth/email-already-in-use":
+          errorMessage = "Este email ja esta sendo utilizado";
+          break;
+        case "auth/internal-error":
+          errorMessage = "Erro no Servidor";
+          break;
+      }
+    }
+    return NextResponse.json(
+      { error: responseMessage },
+      { status: statusCode }
+    );
+  }
 }
-    return NextResponse.json({error: responseMessage}, {status: statusCode};
-}
-}	
